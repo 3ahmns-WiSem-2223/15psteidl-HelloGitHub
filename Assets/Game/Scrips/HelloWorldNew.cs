c6218fcd0c8062875e7e00cde0c80f4c3e05dc60
Meprex <62242723+NinjaLinkHyper@users.noreply.github.com>
2022-09-28 17:36:01 +0200

    Add files via upload

---
+++ Assets/Game/Scrips/HelloWorldNew.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HelloWorldNew : MonoBehaviour
+{
+
+    int Tag;
+
+    void Start()
+    {
+        Debug.Log("Hello World");
+
+        Tag = 9;
+
+
+        Debug.Log(Tag);
+    }
+}
